85934b32e420de83b1710ccfc321ed30eeed8c22
dowdje <dowdje89@gmail.com>
2016-07-31 20:14:54 -0400

    Completed ruby-inheritance-lab-we-0716

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+require_relative "../lib/user"
+class Student < User
+  attr_reader :knowledge
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(nugget)
+    @knowledge << nugget
+  end
+
 
 end
